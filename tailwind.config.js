7c3c77f346d63d672fa36c5d861b555440692edf
ihascats <s.pavlovic.rs@gmail.com>
2022-10-15 12:30:37 +0200

    Create and edit react boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  'tailwindCSS.includeLanguages': {
+    javascript: 'javascript',
+    html: 'HTML',
+  },
+  'editor.quickSuggestions': {
+    strings: true,
+  },
+};
