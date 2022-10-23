26aed13339911e22ee8a9151a328857360e5b5ad
ihascats <s.pavlovic.rs@gmail.com>
2022-10-23 13:26:17 +0200

    Fix game title going to new line if too long

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -18,11 +18,11 @@ function ReviewCard({ review }) {
           ></img>
         )}
         {review.published ? (
-          <h2 className=" bg-slate-900 text-stone-100 font-semibold px-1">
+          <h2 className=" bg-slate-900 text-stone-100 font-semibold px-1 overflow-clip whitespace-nowrap text-ellipsis">
             {review.game_title}
           </h2>
         ) : (
-          <h2 className=" bg-zinc-700 text-zinc-300 font-semibold px-1">
+          <h2 className=" bg-zinc-700 text-zinc-300 font-semibold px-1 overflow-clip whitespace-nowrap text-ellipsis">
             {review.game_title}
           </h2>
         )}
