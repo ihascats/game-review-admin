c8dc9aadd19025d4c6f4e651d29babe75f5ddfe7
ihascats <s.pavlovic.rs@gmail.com>
2022-10-23 15:34:00 +0200

    Change steam price from N/A to Free

--- src/Review.js
+++ src/Review.js
@@ -45,7 +45,7 @@ function Review() {
               currency: 'USD',
               discount_percent: 0,
               final: 0,
-              final_formatted: 'N/A',
+              final_formatted: 'Free',
               initial: 0,
               initial_formatted: '',
             },
